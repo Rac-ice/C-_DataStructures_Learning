f8f4b1b6780fd7fb8d1dceb3b1226ec72f2c0fde
wpj <1602572675@qq.com>
2023-04-10 00:03:16 +0800

    线性表顺序表

---
+++ SqlList/SqlListClass.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlList
+{
+    public  class SqlListClass
+    {
+        const int MaxSize = 100;
+        public string[] data;
+        public int length;
+
+        public SqlListClass()
+        {
+            data = new string[MaxSize];
+            length = 0;
+        }
+
+        public void CreateList(string[] split)
+        {
+            for (int i = 0; i < split.Length; i++)
+            {
+                data[i] = split[i];
+                length = i + 1;
+            }
+        }
+
+        public string DisplayList()
+        {
+            if (length > 0)
+            {
+                string mystr = string.Empty;
+                for (int i = 0; i < length; i++)
+                {
+                    mystr += data[i] + " ";
+                }
+                return mystr;
+            }
+            else
+            {
+                return "空串";
+            }
+        }
+
+        public int ListLength()
+        {
+            return length;
+        }
+
+        public bool GetElem(int i, ref string e)
+        {
+            if (i < 1 || i > length)
+            {
+                return false;
+            }
+            e = data[i - 1];
+            return true;
+        }
+
+        public int LocateElem(string e)
+        {
+            int i = 0;
+            while (i < length && !data[i].Equals(e))
+            {
+                i++;
+            }
+            if (i >= length)
+            {
+                return 0;
+            }
+            else
+            {
+                return i + 1;
+            }
+        }
+
+        public bool ListInsert(int i, string e)
+        {
+            if (i < 1 || i > length + 1)
+            {
+                return false;
+            }
+            for (int j = length; j >= i; j--)
+            {
+                data[j] = data[j - 1];
+            }
+            data[i - 1] = e;
+            length++;
+            return true;
+        }
+
+        public bool ListDelete(int i, ref string e)
+        {
+            if (i < 1 || i > length)
+            {
+                return false;
+            }
+            for (int j = i - 1; j < length - 1; j++)
+            {
+                data[j] = data[j + 1];
+            }
+            length--;
+            return true;
+        }
+
+        public bool ListRemoveElem(string e)
+        {
+            int i = 0;
+            while (i < length && !data[i].Equals(e))
+            {
+                i++;
+            }
+            if (i > length - 1)
+            {
+                return false;
+            }
+            else
+            {
+                for (int j = i; j < length; j++)
+                {
+                    data[j] = data[j + 1];
+                }
+                length--;
+                return true;
+            }  
+        }
+
+        public void Reverse(ref SqlListClass L)
+        {
+            string tmp = string.Empty;
+            for (int i = 0; i < L.length / 2; i++)
+            {
+                tmp = L.data[i];
+                L.data[i] = L.data[L.length - i - 1];
+                L.data[L.length - i - 1] = tmp;
+            }
+        }
+
+        public void Merge2(SqlListClass L1, SqlListClass L2, ref SqlListClass L3)
+        {
+            int i = 0,  j = 0, k = 0;
+            while (i < L1.length && j < L2.length)
+            {
+                if (Convert.ToInt32(L1.data[i]) < Convert.ToInt32(L2.data[j]))
+                {
+                    L3.data[k] = L1.data[i];
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    L3.data[k] = L2.data[j];
+                    j++;
+                    k++;
+                }
+            }
+            while (i < L1.length)
+            {
+                L3.data[k] = L1.data[i];
+                i++;
+                k++;
+            }
+            while (j < L2.length)
+            {
+                L3.data[k] = L2.data[j];
+                j++;
+                k++;
+            }
+            L3.length = k;
+        }
+
+        public void OddAndEven(SqlListClass L1, ref SqlListClass L2, ref SqlListClass L3)
+        {
+             int i = 0, j = 0, k = 0;
+            for (i = 0; i < L1.length; i++)
+              {
+                if (Convert.ToInt32(L1.data[i]) % 2 != 0)
+                {
+                    L2.data[j] = L1.data[i];
+                    j++;
+                }
+                else
+                {
+                    L3.data[k] = L1.data[i];
+                    k++;
+                }
+            }
+             L2.length = j;
+            L3.length = k;
+        }
+    }
+}
---
+++ SqlListForm/SqlList.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SqlList;
+
+namespace SqlListForm
+{
+    public partial class SqlList : Form
+    {
+        public SqlList()
+        {
+            InitializeComponent();
+        }
+
+        SqlListClass sqlList;
+
+        private void SqlList_Load(object sender, EventArgs e)
+        {
+            sqlList = new SqlListClass();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] str = textBox1.Text.Trim().Split('，', ',');
+            sqlList.CreateList(str);
+            MessageBox.Show("创建成功");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = sqlList.DisplayList();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = sqlList.ListLength();
+            textBox3.Text = i.ToString().Trim();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox5.Text.Trim());
+            string s = string.Empty;
+            bool b = sqlList.GetElem(i, ref s);
+            textBox4.Text = s.Trim();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int i = sqlList.LocateElem(textBox6.Text.Trim());
+            textBox7.Text = i.ToString().Trim();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox8.Text.Trim());
+            string s = textBox9.Text.Trim();
+            bool b = sqlList.ListInsert(i, s);
+            if (b)
+            {
+                textBox10.Text = sqlList.DisplayList().ToString();
+            }
+            else
+            {
+                MessageBox.Show("插入失败");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox11.Text.Trim());
+            string s = string.Empty;
+            bool b = sqlList.ListDelete(i, ref s);
+            if (b)
+            {
+                textBox12.Text = sqlList.DisplayList().ToString();
+            }
+            else
+            {
+                MessageBox.Show("删除失败");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            sqlList.Reverse(ref sqlList);
+            textBox13.Text = sqlList.DisplayList().ToString();
+        }
+
+        SqlListClass SL1, SL2, SL3;
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string[] str = textBox15.Text.Trim().Split('，', ',');
+            SL2 = new SqlListClass();
+            SL2.CreateList(str);
+            MessageBox.Show("创建成功");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            string s = textBox18.Text.Trim();
+            bool b = sqlList.ListRemoveElem(s);
+            if (b)
+            {
+                textBox17.Text = sqlList.DisplayList();
+            }
+            else
+            {
+                MessageBox.Show("删除失败");
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            SqlListClass odd = new SqlListClass();
+            SqlListClass even = new SqlListClass();
+            sqlList.OddAndEven(sqlList, ref odd, ref even);
+            textBox19.Text = odd.DisplayList().ToString();
+            textBox20.Text = even.DisplayList().ToString();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            SL3 = new SqlListClass();
+            SL3.Merge2(SL1, SL2, ref SL3);
+            textBox16.Text = SL3.DisplayList();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string[] str = textBox14.Text.Trim().Split('，', ',');
+            SL1 = new SqlListClass();
+            SL1.CreateList(str);
+            MessageBox.Show("创建成功");
+        }
+    }
+}
